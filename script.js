029ce8388e3faa914953b95d544ef7040f7a58cb
edyang1 <edwardyang609@gmail.com>
2024-08-22 20:59:25 -0400

    refactor: Update landing page header and about section content, and add dynamic typing effect to landing page header and back to top button

--- script.js
+++ script.js
@@ -1,32 +1,6 @@
-// Initialize variables to keep track of the scroll position and direction
-let lastScrollTop = 0;
-
-// Function to show or hide the Back to Top button based on scroll direction
-window.onscroll = function() {
-    var backToTopButton = document.getElementById("back-to-top");
-    let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-    // Check if the user is scrolling up
-    if (currentScrollTop < lastScrollTop && currentScrollTop > 200) {
-        // User is scrolling up, show the button
-        backToTopButton.classList.add("visible");
-    } else {
-        // User is scrolling down or hasn't scrolled enough, hide the button
-        backToTopButton.classList.remove("visible");
-    }
-
-    // Update the last scroll position
-    lastScrollTop = currentScrollTop;
-};
-
-// Smooth scrolling for the Back to Top button
-document.querySelector('#back-to-top').addEventListener('click', function(e) {
-    e.preventDefault();
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
+// *************
+// TYPING EFFECT
+// *************
 
 document.addEventListener("DOMContentLoaded", function() {
     const titles = [
